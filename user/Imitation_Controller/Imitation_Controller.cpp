f50aacf94d0d7de830b583ebce47d9a557c9b39f
nolie-rolie <nfey@nd.edu>
2023-07-07 15:13:41 -0400

    tuned ddp feedback for hardware test

--- user/Imitation_Controller/Imitation_Controller.cpp
+++ user/Imitation_Controller/Imitation_Controller.cpp
@@ -317,10 +317,9 @@ void Imitation_Controller::locomotion_ctrl()
     // compute ddp feedback control
     body_state.segment<3>(0) << seResult.rpy[2], seResult.rpy[1], seResult.rpy[0];
     body_state.segment<3>(3) = seResult.position;
-    body_state.segment<3>(6) << seResult.omegaWorld[2], seResult.omegaWorld[1], seResult.omegaWorld[0];
+    body_state.segment<3>(6) << seResult.omegaBody[2], seResult.omegaBody[1], seResult.omegaBody[0];
     body_state.segment<3>(9) = seResult.vWorld;
     Vec12<float> ddp_feedback = ddp_feedback_gains * (des_body_state - body_state);
-    Vec3<float> f_ff_feedback[4];
 
     // compute foot position and predicted GRF
     for (int l = 0; l < 4; l++)
@@ -330,7 +329,7 @@ void Imitation_Controller::locomotion_ctrl()
                    seResult.rBody.transpose() * (_quadruped->getHipLocation(l) + _legController->datas[l].p);
 
         // friction cone check
-        f_ff[l] = mpc_control.segment(3 * l, 3).cast<float>() - 0.2 * ddp_feedback.segment(3 * l, 3).cast<float>();
+        f_ff[l] = mpc_control.segment(3 * l, 3).cast<float>() - 0.1 * ddp_feedback.segment(3 * l, 3).cast<float>();
         if (f_ff[l][2] < 0){
             std::cout << "Zeroed force on leg " << l << " (fz < 0)." << std::endl;
             std::cout << "   Force: " << f_ff[l].transpose() << std::endl;
