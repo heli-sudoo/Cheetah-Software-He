b42edbd2b4fac2690c151640ef4a96cb52982e20
nolie-rolie <nfey@nd.edu>
2023-07-25 19:49:10 -0400

    fixed mistakes in terrain euler angle ordering + added higher swing gains when jumping onto angled terrain

--- user/Imitation_Controller/Imitation_Controller.cpp
+++ user/Imitation_Controller/Imitation_Controller.cpp
@@ -437,8 +437,8 @@ void Imitation_Controller::locomotion_ctrl()
         pDesLegFinal[i] = pf_rel_com_filtered[i] - seResult.rBody.transpose()*_quadruped->getHipLocation(i);
         
         // float min_foot_dist = 0.25 - 0.05 * (center_point[2] > 1e-3);
-        float min_foot_dist = 0.25 + (-0.1 + 0.05 * (abs(eul_terrain[0]) > 1e-3 || abs(eul_terrain[1]) > 1e-3)) * (pf_init[i][2] < pf_filtered[i][2]);
-        float max_foot_dist = 0.4 - (0.05 +  0.05 * (abs(prev_eul_terrain[0]) > 1e-3 || abs(prev_eul_terrain[1]) > 1e-3)) *  (pf_init[i][2] > pf_filtered[i][2]);    
+        float min_foot_dist = 0.25 + (-0.1 + 0.05 * (abs(eul_terrain[1]) > 1e-3 || (abs(eul_terrain[2]) > 1e-3))) * (pf_init[i][2] < pf_filtered[i][2]);
+        float max_foot_dist = 0.4 - (0.05 +  0.05 * (abs((prev_eul_terrain[1]) > 1e-3) || (abs(prev_eul_terrain[2]) > 1e-3))) *  (pf_init[i][2] > pf_filtered[i][2]);    
         if (contactStatus[0] && contactStatus[1] && contactStatus[2] && contactStatus[3])
         {
             foot_offsets[i].setZero();
@@ -601,7 +601,11 @@ void Imitation_Controller::locomotion_ctrl()
 
             // feedback terms: exponential error response
             // float wn = 20.0;
-            float wn = 50.0;
+            // float wn = 50.0;
+            float wn = 20.0;
+            if ((abs(eul_terrain[1]) > 1e-3) || (abs(eul_terrain[2]) > 1e-3)){
+                wn = 50.0;
+            }
             float KpFoot = wn * wn;
             float KdFoot = 2 * wn;
             Vec3<float> pFootHip = _legController->datas[i].p;
